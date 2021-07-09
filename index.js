a08edefb3485ccba72d9a60c1a1faf68b59c3ea0
egovar <egovar371@gmail.com>
2021-07-10 00:32:04 +0300

    maybe this will work

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const path = require('path');
-const serveStatic = require('serve-static');
 const app = express();
-app.use(serveStatic(__dirname + "/dist"));
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server running at port ${port}/`);
+});
+
+app.get('/:folder/:file', (req, res) => {
+    res.sendFile(__dirname + `/dist/` + req.params.folder + '/'+ req.params.file);
+});
+app.get('/:file', (req, res) => {
+    res.sendFile(__dirname + `/dist/` + req.params.file);
+});
+app.get('/*', (req, res) => {
+    res.sendFile(__dirname + `/dist/index.html`);
 });
 
