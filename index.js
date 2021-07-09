2b6dca01aa5db115a391a283b647a86e33bccadf
egovar <egovar371@gmail.com>
2021-07-10 00:09:41 +0300

    lets try this

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist'));
-let port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started ' + port);
