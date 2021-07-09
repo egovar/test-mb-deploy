1e06dcb41465adcda46c18d32c94f0d1eccec391
egovar <egovar371@gmail.com>
2021-07-10 00:25:11 +0300

    maybe this will work

---
+++ index.js
@@ -0,0 +1,11 @@
+const pxpress = require('express');
+const path = require('path');
+const perveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server running at port ${port}/`);
+});
+
